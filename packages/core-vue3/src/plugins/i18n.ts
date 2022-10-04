21d524d9742cf1e8ce0a3a404762ad48c2b8e924
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-10-04 22:28:30 +0200

    Use default i18n values

--- packages/core-vue3/src/plugins/i18n.ts
+++ packages/core-vue3/src/plugins/i18n.ts
@@ -1,7 +1,10 @@
 import { App } from "vue";
 import { createI18n } from "vue-i18n";
+import merge from 'deepmerge'
 
 import { MilochauCoreOptions } from "../types/options";
+import en from '../data/en.json'
+import fr from '../data/fr.json'
 
 const coreCreateI18nPlugin = {
   install(app: App, options: MilochauCoreOptions) {
@@ -9,14 +12,18 @@ const coreCreateI18nPlugin = {
     const i18n = createI18n({
       locale: 'en',
       fallbackLocale: 'en',
-      messages: options.messages,
+      messages: merge(options.messages,
+        {
+          en,
+          fr
+        }),
       legacy: false
     })
 
     app.use(i18n)
-  
+
     return i18n
-    
+
   }
 }
 
