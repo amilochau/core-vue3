8928cf865bbc8ac2652fd99719e9acf35a70d4f6
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2022-11-02 18:07:15 +0100

    Don't require authentication for Privacy page

--- packages/core-vue3/src/data/routes.ts
+++ packages/core-vue3/src/data/routes.ts
@@ -1,7 +1,7 @@
 import { RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
-  { name: 'Privacy', path: '/:lang([a-z]{2})/privacy', component: () => import('../pages/PagePrivacy.vue'), meta: { requiresAuth: true } },
+  { name: 'Privacy', path: '/:lang([a-z]{2})/privacy', component: () => import('../pages/PagePrivacy.vue') },
   { name: 'Forbidden', path: '/:lang([a-z]{2})/forbidden', component: () => import('../pages/PageForbidden.vue') },
   { name: 'NotFound', path: '/:lang([a-z]{2})/:pathMatch(.*)*', component: () => import('../pages/PageNotFound.vue') },
 ]
