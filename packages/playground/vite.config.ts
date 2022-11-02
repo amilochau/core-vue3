571636f826ad42c918cd80a6c34b377e6bfae2ac
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2022-11-02 17:25:50 +0100

    Use vuetify v3 stable version

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import ViteFonts from 'vite-plugin-fonts'
 import { setDefaultResultOrder } from 'dns'
 
 setDefaultResultOrder('verbatim')
@@ -10,6 +11,14 @@ export default defineConfig({
   plugins: [
     vue(),
     vuetify(),
-    vueI18n()
+    vueI18n(),
+    ViteFonts({
+      google: {
+        families: [{
+          name: 'Roboto',
+          styles: 'wght@100;300;400;500;700;900',
+        }],
+      },
+    })
   ]
 })
