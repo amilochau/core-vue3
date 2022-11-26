1dae3980315fc4c3be4e3dfe04de099aba30d686
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-11-26 11:28:25 +0100

    Fix 'AuthPolicy' export

--- packages/core-vue3/src/types/http/index.ts
+++ packages/core-vue3/src/types/http/index.ts
@@ -1,9 +1,13 @@
-import { IHttpSettings } from "./IHttpSettings";
+import { AuthPolicy, IHttpSettings } from "./IHttpSettings";
 import { IProblemDetails } from "./IProblemDetails";
 
 export type {
   IHttpSettings,
   IProblemDetails
+}
+
+export {
+  AuthPolicy
 }
 
 export * from './ListPayload'
