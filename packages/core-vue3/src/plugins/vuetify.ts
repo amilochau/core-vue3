3c590ebdf5fa594adbc7cca63cc3544ba4285e14
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-11-30 13:59:35 +0100

    Define vuetify options from plugin options

--- packages/core-vue3/src/plugins/vuetify.ts
+++ packages/core-vue3/src/plugins/vuetify.ts
@@ -5,11 +5,22 @@ import { MilochauCoreOptions } from '../types/options'
 import { createVuetify } from 'vuetify'
 import { en, fr } from 'vuetify/locale'
 import { aliases, mdi } from 'vuetify/lib/iconsets/mdi-svg'
+import { VuetifyOptions } from 'vuetify/framework'
+import merge from 'deepmerge'
 
 export default {
   install: (app: App, options: MilochauCoreOptions) => {
 
-    const vuetify = createVuetify({
+    const defaultVuetifyOptions: VuetifyOptions = {
+      theme: {
+        themes: {
+          light: {
+            colors: {
+              background: '#fcfcfc'
+            }
+          }
+        }
+      },
       icons: {
         defaultSet: 'mdi',
         aliases,
@@ -23,7 +34,10 @@ export default {
           fr
         }
       }
-    })
+    }
+
+    const vuetifyOptions = options.vuetify ? merge(defaultVuetifyOptions, options.vuetify) : defaultVuetifyOptions
+    const vuetify = createVuetify(vuetifyOptions)
 
     app.use(vuetify)
 
