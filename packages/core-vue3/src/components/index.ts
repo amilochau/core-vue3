e92425ef6323683d948c743b4baf642b48c19086
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-14 19:52:38 +0100

    Add a CardMessage component to display messages in a dialog (#43)

--- packages/core-vue3/src/components/index.ts
+++ packages/core-vue3/src/components/index.ts
@@ -1,11 +1,13 @@
 import CardActions from "./cards/CardActions.vue"
 import CardDivider from "./cards/CardDivider.vue"
+import CardMessages from "./cards/CardMessages.vue"
 import CardSectionTitle from "./cards/CardSectionTitle.vue"
 import CardTitleClosable from "./cards/CardTitleClosable.vue"
 
 export {
   CardActions,
   CardDivider,
+  CardMessages,
   CardSectionTitle,
   CardTitleClosable,
 }
