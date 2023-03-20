2795cb2bff575807843e647cf121d93c9dc396f5
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-20 13:19:44 +0100

    Add a validation for date field (#57)

--- packages/core-vue3/src/composition/validation.ts
+++ packages/core-vue3/src/composition/validation.ts
@@ -9,6 +9,7 @@ export function useValidationRules() {
       minLength: "This field must be more than {min} characters.",
       maxLength: "This field must be less than {max} characters.",
       emailAddress: "This field must be a valid email address.",
+      date: "This field must be a valid date.",
       minValue: "This field must be upper than {min}.",
       maxValue: "This field must be lower than {max}."
     }
@@ -19,6 +20,7 @@ export function useValidationRules() {
       minLength: "Ce champ doit faire plus de {min} caractères.",
       maxLength: "Ce champ doit faire moins de {max} caractères.",
       emailAddress: "Ce champ doit correspondre à une adresse email valide.",
+      date: "Ce champ doit correspondre à une date valide.",
       minValue: "Ce champ doit être supérieur à {min}.",
       maxValue: "Ce champ doit être inférieur à {max}."
     }
@@ -34,6 +36,7 @@ export function useValidationRules() {
   const minLength = (min: number) => (v: any) => !v || v.length >= min || t('validation.minLength', { min })
   const maxLength = (max: number) => (v: any) => !v || v.length <= max || t('validation.maxLength', { max })
   const emailAddress = () => (v: any) => !v || /.+@.+\..+/.test(v) || t('validation.emailAddress')
+  const date = () => (v: any) => !v || (!isNaN(Date.parse(v)) && /^\d{4}-\d{2}-\d{2}$/.test(v)) || t('validation.date')
   const minValue = (min: number) => (v: any) => {
     if (v === null || v === undefined) {
       return true;
@@ -54,6 +57,7 @@ export function useValidationRules() {
     minLength,
     maxLength,
     emailAddress,
+    date,
     minValue,
     maxValue
   }
