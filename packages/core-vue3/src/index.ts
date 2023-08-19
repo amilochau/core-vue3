395c18e32488a24d8b3583e7c557c7dfdf1678b2
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2023-08-19 18:03:12 +0200

    Fix app creation: create only one application

--- packages/core-vue3/src/index.ts
+++ packages/core-vue3/src/index.ts
@@ -1,4 +1,4 @@
-import { type App, type Component, createApp as createClientApp } from 'vue'
+import { type App, createApp as createClientApp } from 'vue'
 import type { Router } from 'vue-router'
 import type { Pinia } from 'pinia'
 import type { MilochauCoreOptions } from './types/options'
@@ -8,17 +8,17 @@ import { registerHead } from './plugins/head'
 import { registerVuetify } from './plugins/vuetify'
 import { registerPinia } from './plugins/pinia'
 import { registerRouter } from './plugins/router'
+import PageApp from './pages/PageApp.vue'
 
 // Styles
 import './styles/main.scss'
 
 export const CoreVue3 = (
-  App: Component,
   options: MilochauCoreOptions,
   fn?: (context: { app: App, pinia: Pinia, router: Router }) => Promise<any>,
 ) => {
   const createApp = async () => {
-    const app = createClientApp(App);
+    const app = createClientApp(PageApp);
     
     app.provide('core-options', options)
 
@@ -36,6 +36,10 @@ export const CoreVue3 = (
 
     app.use(router);
     
+    // wait until page component is fetched before mounting
+    await router.isReady()
+    app.mount("#app", true)
+    
     return {
       app,
       head,
@@ -45,14 +49,7 @@ export const CoreVue3 = (
       vuetify,
     }
   }
-
-  (async () => {
-    const { app, router } = await createApp()
-    // wait until page component is fetched before mounting
-    await router.isReady()
-    app.mount("#app", true)
-  })()
-
+  
   return createApp
 }
 
