7a26345a8f6d3b4b4f5f23031061c8314aa9ba59
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2024-06-11 18:14:35 +0200

    Expose utils: `clone`, `deepEqual`, `getNewKey`

---
+++ packages/core-vue3/src/utils/index.ts
@@ -0,0 +1,3 @@
+export * from './clone';
+export * from './deepEqual';
+export * from './records';
---
+++ packages/core-vue3/src/utils/records.ts
@@ -0,0 +1,15 @@
+export const getNewKey = <TItem>(record: Record<string, TItem>, maxIndex: number = 100) => {
+  let index = 0;
+
+  while (index < maxIndex) {
+    const indexAsString = `${index}`;
+    if (!record[index]) {
+      // The current index is free
+      return indexAsString;
+    }
+    index++;
+  }
+
+  // No index is free
+  throw new Error('Can\'t create new item!');
+};
